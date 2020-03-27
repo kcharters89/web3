1e6cbc900c17f2a31d3a013f8d6228ad9b0f547a
chartkr1 <chartkr1@student.op.ac.nz>
2020-03-28 11:53:45 +1300

    jinja template nav template

---
+++ app1/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request
+from mongoengine import *
+import json
+import os
+import csv
+import jinja2
+
+connect('web3')
+
+app = Flask(__name__)
+#before the routes , later on will go in a seperate file for structure
+app.config.from_object('config')
+
+
+class Country(Document):
+    name = StringField()
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def index():
+    #choose csv that doeesnt have missing data or figure how turn blanks into 0's
+    for file in os.listdir(app.config['FILES_FOLDER']):
+        filename = os.fsdecode(file)
+        path = os.path.join(app.config['FILES_FOLDER'],filename)
+        f = open(path)
+        r = csv.reader(f)
+        d = list(r)
+        for data in d:
+            print(data)
+    return render_template('index.html')
+   
+#add new countries in the routes so it doenst run every time the app is run 
+#for some reason wont save to db ?? 
+@app.route('/inspiration')
+def inspiration():
+    return render_template('inspiration.html')
+
+@app.route('/loadData')
+def utility():
+    nz = Country(name="New Zealand").save()
+    aus = Country(name="Australia").save()
+    fra = Country(name="France").save()
+    germ = Country(name="Germany").save()
+    names = []
+    for c in Country.objects:
+       names.append(c.name)
+       
+    #json will turn it into a string 
+    #return Country.objects.to_json() built into mongoengine
+    return json.dumps(names) 
+    #json.dumps(names) #python one 
+
+#api's (seperate file?)
+@app.route('/countries', methods=['GET'])
+@app.route('/countries/<countries_id>', methods=['GET'])
+def getCountries(countries_id=None):
+    countries = None
+    #figure out what to do with the countries id idk im struggling
+    if countries_id is None:
+        countries = Country.objects
+    else:
+        countries = Country.objects.get(name=countries_id)
+    return countries.to_json()
+     
+if __name__ =="__main__":
+    #app.run(debug=True, port=8080)
+    app.run(host='10.25.137.221', port=80)
+    
+
+
+	
