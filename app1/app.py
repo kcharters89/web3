a4716c6082f41867ce220218f5f0755533b007da
kcharters89 <chartkr1@student.op.ac.nz>
2020-04-18 14:40:29 +1200

    Update app.py

--- app1/app.py
+++ app1/app.py
@@ -103,8 +103,8 @@ def getCountries(countries_id=None):
         return render_template('countries.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
-    #app.run(host='0.0.0.0', port=80)
+    #app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', port=80)
     
 
 
