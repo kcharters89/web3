72dfc6e9718b4953054a0ee69ccd16cff2023308
chartkr1 <chartkr1@student.op.ac.nz>
2020-05-12 09:11:37 +1200

    still working on d3 , ajax working

--- app1/app.py
+++ app1/app.py
@@ -16,25 +16,26 @@ app.config.from_object('config')
 #models?
 class Country(Document):
     name = StringField()
+    data = DictField()
     
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
 def index():
+    #put in load data
     #choose csv that doeesnt have missing data or figure how turn blanks into 0's
-    my_dict = {}
-    for file in os.listdir(app.config['FILES_FOLDER']):
-        filename = os.fsdecode(file)
-        path = os.path.join(app.config['FILES_FOLDER'],filename)
-        f = open(path)
-        r = csv.reader(f)
-        d = list(r)
-        for row in d:
-            my_dict[row[0]] = [cell for cell in row[1:] if cell]
-        print(my_dict)    
-    #works for only zimbabwae by turning blank into 0 need it for all countries 
-    #ask about this maybe
-    return render_template('index.html')
+    
+        #this below will turn blank into 0  need to incorperate into above code?
+        # for row in d:
+        #     for i, x in enumerate(row):
+        #         if len(x)< 1:
+        #                  x = row[i] = 0
+        #     print (','.join(str(x)for x in row))  
+            
+          
+        
+        
+    return render_template('index.html'), 200
    
 #add new countries in the routes so it doenst run every time the index is run 
 
@@ -44,70 +45,91 @@ def inspiration():
 
 @app.route('/loadData')
 def utility():
-    #this loads the data to database
-    nz = Country(name="New Zealand").save()
-    aus = Country(name="Australia").save()
-    fra = Country(name="France").save()
-    germ = Country(name="Germany").save()
-    names = []
-    
-    
-    for c in Country.objects:
-        if c.name not in Country.objects:
-            names.append(c.name) 
-        else:
-            names.insert(0)    
-    # how to stop duplicates this keeps adding the same list to itself 
-    # #json will turn it into a string 
-    # #return Country.objects.to_json() built into mongoengine
-    return json.dumps(names)
+    for file in os.listdir(app.config['FILES_FOLDER']):
+        filename = os.fsdecode(file)
+        path = os.path.join(app.config['FILES_FOLDER'],filename)
+        f = open(path)
+        r = csv.DictReader(f) 
+        d = list(r)
+        for data in d:
+            country = Country() # a blank placeholder country
+            dict = {} # a blank placeholder data dict
+            for key in data: # iterate through the header keys
+                if key == "country":
+                    # check if this country already exists in the db
+                    countryName = data[key]
+                    # if the country does not exist, we can use the new blank country we created above, and set the name
+                    #check database with Countrys.objects thats what this is 
+                    if Country.objects(name = countryName).count() == 0:
+                        country['name'] = countryName
+                        country.save()
+                    else:    
+                    # if the country already exists, replace the blank country with the existing country from the db, and replace the blank dict with the current country's 
+                    # data  
+                        country = Country.objects.get(name=countryName)
+                        dict = country['data']
+                else:
+                    f = filename.replace(".csv","") # we want to trim off the ".csv" as we can't save anything with a "." as a mongodb field name
+                    if f in dict: # check if this filename is already a field in the dict
+                        dict[f][key] = data[key] # if it is, just add a new subfield which is key : data[key] (value)
+                    else:
+                        dict[f] = {key:data[key]} # if it is not, create a new object and assign it to the dict
+
+                # add the data dict to the country
+                country['data'] = dict
+            # save the country
+            country.save()
+    return Country.objects.to_json()
     #json.dumps(names) #python one 
 
 #api's (seperate file?)
-@app.route('/countries<countries_id>', methods=['GET'])
-def getCountriesById(countries_id=None):
-    countries = None
-    if countries_id is None:
-        countries = Country.objects
-    else:
-        countries = Country.objects.get(name=countries_id)
-    return countries.to_json()
-
+@app.route('/countries/<countries_id>', methods=['GET'])
 @app.route('/countries', methods=['GET','POST','DELETE'])
 def getCountries(countries_id=None):
     if request.method == 'GET':
+
         name=request.form.get('name') 
-        if countries_id is None:
-            countries = Country.objects
-             
+
+        if countries_id is None: # if no country given
+            countries = Country.objects #return all
+            if name: #if name enetered
+                 countries = Country.objects.get(name=name) #return objects with that name
+            else:
+                countries = Country.objects #othrwise return all
         else:
-            countries = Country.objects.get(name=name)
-            
+            countries = Country.objects(name=countries_id).all()   #if non of the above return all     
         
-        # render_template('countries.html', countries=countries)
+        
          
     elif request.method == 'POST':
         #updating data in database
-        
+        #on postman can add empty data and thats it
         name=request.form.get('name')
-        
-        newName = Country(name=name).save()
+        if name:
+            newName = Country(name=name).save()
         
         countries = Country.objects
         
-        #render_template('countries.html',countries=countries, name=name)
-
+       
+        #delete doesnt work anymore
     elif request.method == 'DELETE':
          
         name = request.form.get('name') 
-        delname = Country.objects(name=name).all()
+        #gets one
+        delname = Country.objects.get(name=name)
         delname.delete()
         countries = Country.objects
         
-    else:
-    # POST Error 405 Method Not Allowed
-        return render_template('countries.html')
-    return render_template('countries.html',countries=countries, name=name)
+        
+       
+    return countries.to_json()
+    #for countries data 
+@app.route('/countrypage')
+def countryPage():
+    countries = Country.objects
+    return render_template('countries.html',countries=countries)
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
     #app.run(host='0.0.0.0', port=80)
--- app1/static/ajax.js
+++ app1/static/ajax.js
@@ -1,37 +1,76 @@
 $(document).ready(function() {
-    $.ajax({
-        url: "/countries",
-        type:"GET",
-        success: function(result){
-            alert('ok');
-        },
-        error:function(error){
-            alert('error');
-        }
-   });
-$('#button').click(function(){
-    $.ajax({
-        type:"POST",
-        url:"/countries",
-        data: $('form').serialize(),
-        success: function(result) {
-            alert('ok');
-          },
-          error: function(result) {
-            alert('error');
-          }
-    })
-})
-$('.wrapper').on('click', '.delete_row', function(){
-    var val = $('.input_row').val();
+  fetch('/countries')
+  .then(response => response.json())
+  .then(data => {
+   
+  //having issue with scale and domain range
+  d3.max(data, function(d) { 
+    console.log(d.data[0])   //Returns undefined
+    return d[0];  //References first value in each sub-array
+});
+
+  var myScale = d3.scaleLinear()
+  .domain([0, 10])
+  .range([0, 600]);
+ console.log (myScale(5))//outputs 300
+
+    var g = d3.select("svg").selectAll("g").data(data);
+                   
+    var en = g.enter().append("g")
+    .attr("transform",function(d){ 
+      //replace math with d. something with your data console log the d no need to loop
+    //console.log(d.data.child_mortality_0_5_year_olds_dying_per_1000_born)
+    //translate expects a number but i dont know what number
+  // x and y coords
+  
+  //d.data.child_mortality_0_5_year_olds_dying_per_1000_born[] income_per_person_gdppercapita_ppp_inflation_adjusted[]
+  //domain and range look into this , mapping function
+  
+    return "translate("+ (Math.random() * 100) + 40 + "," + (Math.random() * 100) + 40 +")" 
+    
+  });
+// made colourful circles need to join with text?
+    var circle = en.append("circle")
+    .attr("cx", function(d,i){return 30 + i*60})
+    .attr("cy", 250).attr("r", 19)
+    .attr("fill", function(d,i){return d3.schemeCategory10[i % 10] })
+    en.append("text").text(function(d){ return d.name });
+  
+  });
+
+$('#sub_button').click(function(){
+  var name = $('#countryName').val()
     $.ajax({
       url: "/countries",
-      type: "DELETE",
-      data: {id: val},
-      success: function(response) {
-        $('.input_row').val('');
-      },
-
+      type:"POST",
+      data:{name:name},
+    
+    }).done(function(response){
+    console.log(response)
+    }).fail(function(response){
+    console.log(response)
+    }).always(function(response){
+    console.log(response)
+    
     });
   });
+    
+$('#del_button').click(function(){
+
+  var name = $('#countryName').val()
+    $.ajax({
+      url: "/countries",
+      type:"DELETE",
+     
+    }).done(function(response){
+    console.log(response)
+    }).fail(function(response){
+    console.log(response)
+    }).always(function(response){
+    console.log(response)
+    
+    });
+  }) ;
+
+
 });
